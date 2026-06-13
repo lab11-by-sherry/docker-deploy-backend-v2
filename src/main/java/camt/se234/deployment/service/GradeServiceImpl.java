d2d473b953d3fc0a7221f93a889ad13f62063606
Shumei Xu <140968472+mayhaoo@users.noreply.github.com>
2026-06-13 20:52:56 +0800

    add docker-compose-prod

--- src/main/java/camt/se234/deployment/service/GradeServiceImpl.java
+++ src/main/java/camt/se234/deployment/service/GradeServiceImpl.java
@@ -7,17 +7,9 @@ import org.springframework.stereotype.Service;
 public class GradeServiceImpl implements GradeService {
     @Override
     public String getGrade(double score) {
-        if (score > 79.5) {
-            return "A";
+        if (score > 60) {
+            return "pass";
         }
-        else if (score > 74.5){
-            return "B";
-        }else if (score > 59.5){
-            return "C";
-        }else if (score > 32.5) {
-            return "D";
-        } else
-            return "F";
+        else return "dont pass";
 
-    }
 }
